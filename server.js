9645b5138387a2e754bf5d34a0db23ed7e186132
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-06 16:42:10 -0400

    database created

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+
+// to ensure we can read secrets
+// from the .env file
+require('dotenv').config();
+// runs mongoose.connect code
+require('./config/database')
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+
+
+// Configure both serve-favicon & static middlewares
+// to serve from the production 'build' folder
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Put API routes here, before the "catch all" route
+///////////
+//////////
+
+
+// The following "catch all" route (note the *)is necessary
+// for a SPA's client-side routing to properly work
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+
+
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3001;
+
+app.listen(port, function() {
+  console.log(`Express app running on port ${port}`)
+});
