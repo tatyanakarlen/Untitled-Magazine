08b8344b90ebd8e995dd01d9a3576b8588e87dd8
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-10 12:07:47 -0400

    merged with new main section

--- src/pages/AdminDashboardPage/AdminDashboardPage.jsx
+++ src/pages/AdminDashboardPage/AdminDashboardPage.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Nav from '../../components/Nav/Nav';
-// import DashboardHeader from '../../components/DashboardHeader/DashboardHeader';
-// import DashboardCardSm from '../../components/DashboardCardSm/DashboardCardSm.jsx';
-// import DashboardCardLg from '../../components/DashboardCardLg/DashboardCardLg.jsx';
 import Main from '../../components/Main/Main.jsx'
 import './AdminDashboardPage.css';
 
