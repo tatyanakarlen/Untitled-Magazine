db29445ab01003d7456f59a0947542cf0d01b490
gabe.vela <77593670+gabrielgomezvela@users.noreply.github.com>
2022-08-19 19:35:59 -0400

    first commit

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 // Configure both serve-favicon & static middlewares
 // to serve from the production 'build' folder
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
 // below routes for POST requests
