d026d631d6f0713486e5f12d4e2536de4f03b4c5
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-19 09:11:29 -0400

    fixing repsonsive

--- src/components/dashboard/BreadCrumb/BreadCrumb.jsx
+++ src/components/dashboard/BreadCrumb/BreadCrumb.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const BreadCrumb = () => {
   return (
     <div>
-      <nav aria-label="breadcrumb">
-                    <ol style={{ backgroundColor: "#ced4da", height: "2.5rem", paddingTop: '0.5rem', paddingLeft: '0.5rem' }}class="breadcrumb">
-                        
+      <nav aria-label="breadcrumb" style={{  width: "100%", margin: '0 auto', marginTop: '-0.75rem' }}>
+                    <ol style={{ backgroundColor: "#ced4da", height: "2.5rem" }}class="breadcrumb">
+                    {/* paddingTop: '-2rem', paddingLeft: '0.5rem' */}
                         <li class="breadcrumb-item"><a href="#">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Overview</li>
                     </ol>
