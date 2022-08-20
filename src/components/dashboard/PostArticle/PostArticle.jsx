833c2660c31cffbf20e941b6964e6678aa331943
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-20 10:57:52 -0400

    photo uploads working

--- src/components/dashboard/PostArticle/PostArticle.jsx
+++ src/components/dashboard/PostArticle/PostArticle.jsx
@@ -3,29 +3,70 @@ import './PostArticle.css'
 import * as Icon from 'react-bootstrap-icons';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { useState, useEffect } from 'react';
+import PostArticleForm from '../PostArticleForm/PostArticleForm'
+import { useState, useEffect, useCallback, useRef } from 'react';
 
 
 
 const PostArticle = ({ user, allArticles, setArticles }) => {
+
+  const ref = useRef();
+  const [img, setImg] = useState();
+  const [imgLink, setImgLink] = useState();
+
 
     const[article, setArticle] = useState({
         title: '',
         contributor: '',
         body: '',
         tags: '',
+        image: '',
         postedBy: user._id, 
       });
+
+      const onImgChange =  useCallback((e) => {
+        const [file] = e.target.files;
+        var reader = new FileReader();
+          reader.readAsDataURL(file); 
+          reader.onloadend = function() {
+            var base64data = reader.result;   
+            setImg(base64data)
+          }
+        
+      }, [])
 
       const postArticle = async (e) => {
         e.preventDefault();
         console.log(article);
-        
+        //Image sending starts here
+        // telling the server - take this photo - send it to cloudinary - and give us the link of where it is stored 
+        //  
         try {
+            
+            let responseOne = await fetch("/img", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                photo: img // take this image, if all goes wel then ....
+            }),
+            })
+        
+            let responseTwo = await responseOne.json()
+        
+            console.log("cloudinary url :", responseTwo.imageurl )
+            setArticle({  // hook  - ? why is this not updating in time for the next api cal ?? 
+                ...article, 
+                image: responseTwo.imageurl   // this is the image url where it is stored and should be sent to the db, but it is 
+                // being stored as an empty string.... why?  
+            })
+          
+          //Image sending ends here
             const res = await fetch('/api/articleSubmissions', {
               method: 'POST',
               headers: { 'Content-Type': 'application/json' },
-              body: JSON.stringify({ article: article }),
+              body: JSON.stringify({ article: article, img: responseTwo.imageurl }),
             });
             console.log(res);
             if (res.statusText === 'OK') {
@@ -35,11 +76,11 @@ const PostArticle = ({ user, allArticles, setArticles }) => {
                 contributor: '',
                 body: '',
                 tags: '',
+                image: '',
                 postedBy: user._id, 
               });
-
-              const newArticlesList = [...allArticles, article]
-              setArticles(newArticlesList)
+              ref.current.value = "";
+              setImg("")
             }
           } catch (err) {
             console.log(err.message);
@@ -48,142 +89,58 @@ const PostArticle = ({ user, allArticles, setArticles }) => {
 
       }
 
-      
+      // const postArticle = async (e) => {
+      //   e.preventDefault();
+      //   console.log(article);
+        
+      //   try {
+      //       const res = await fetch('/api/articleSubmissions', {
+      //         method: 'POST',
+      //         headers: { 'Content-Type': 'application/json' },
+      //         body: JSON.stringify({ article: article }),
+      //       });
+      //       console.log(res);
+      //       if (res.statusText === 'OK') {
+      //         console.log('SUCCESSLY ADDED TO DB =>', article);
+      //         setArticle({
+      //           title: '',
+      //           contributor: '',
+      //           body: '',
+      //           tags: '',
+      //           postedBy: user._id, 
+      //         });
+
+      //         const newArticlesList = [...allArticles, article]
+      //         setArticles(newArticlesList)
+      //       }
+      //     } catch (err) {
+      //       console.log(err.message);
+      //     }
+
+
+      // }
+
     
-    
-    //   const postContributor = async (e) => {
-    //     e.preventDefault();
-    //     console.log(contributor);
-    //     // console.log(user._id);
-
-    //     // '/api/contributorSubmissions'
-
-    //     try {
-    //     const res = await fetch('/api/contributorSubmissions', {
-    //       method: 'POST',
-    //       headers: { 'Content-Type': 'application/json' },
-    //       body: JSON.stringify({ contributor: contributor }),
-    //     });
-    //     console.log(res);
-    //     if (res.statusText === 'OK') {
-    //       console.log('SUCCESSLY ADDED TO DB =>', contributor);
-    //       setContributor({
-    //         name: '',
-    //         email: '',
-    //         city: '',
-    //         country: '',
-    //         postedBy: user._id, 
-    //       });
-    //     }
-    //   } catch (err) {
-    //     console.log(err.message);
-    //   }
-    // };
-
-    // PostArticle
   return (
     <div class="col d-flex flex-column h-sm-100">
         <div id="articlesPost-main-container" class="row overflow-auto">          
         <div id="articlesPost-container" class="row text-center g-3">
-        <div class="col-md"> 
-        <div class="card bg-light text-dark">
-      <div class="card-body text-center">
-        <div class="h1 mb-1">
-        <Icon.ArrowRight />
-        </div>
-        <h5 class="card-title mb-1">Post Article</h5>
-        <form class="row g-3">
-          <div id="post-article-form-box" class="col-md-12">
-              {/* <br/> */}
-             <label for="comments" class="form-label"></label>
+        <PostArticleForm  
+        user={user} 
+        allArticles={allArticles} 
+        setArticles={setArticles} 
+        postArticle={postArticle}
+        article={article} 
+        setArticle={setArticle}
+        img={img}
+        setImg={setImg}
+        imgLink={imgLink}
+        setImgLink={setImgLink}
+        onImgChange={onImgChange}
+         />
 
-             {/* value={submission.name}
-            onChange={(e) =>
-              setSubmission({
-                ...submission,
-                name: e.target.value,
-              })
-            } */}
 
-              <input 
-              class="form-control title-place" 
-              type="text" 
-              name= "title"
-              rows="2" 
-              required placeholder="Title"
-              pattern="[A-Za-z]{2,}"
-              value={article.title}
-              onChange={(e) => 
-                setArticle({
-                    ...article, 
-                    title: e.target.value
-                })
-              }
 
-            
-                 
-              >
-              </input>
-              <label for="comments" class="form-label"></label>
-              <input 
-              class="form-control body-place"  
-              rows="10" cols="40" 
-              type="text" 
-              name= "contributor"
-              required placeholder="Contributor"
-              value={article.contributor}
-              onChange={(e) => 
-                setArticle({
-                    ...article, 
-                    contributor: e.target.value
-                })
-              }
-              >
-              </input>
-              <label for="comments" class="form-label"></label>
-              <textarea 
-              class="form-control body-place"  
-              rows="10" cols="40" 
-              type="text" 
-              name= "body"
-              required placeholder="Article body here"
-              value={article.body}
-              onChange={(e) => 
-                setArticle({
-                    ...article, 
-                    body: e.target.value
-                })
-              }
-              >
-              </textarea>
-              <label for="comments" class="form-label"></label>
-              <input 
-              class="form-control body-place"  
-              rows="10" cols="40" 
-              type="text" 
-              name= "tags"
-              required placeholder="Tags"
-              value={article.tags}
-              onChange={(e) => 
-                setArticle({
-                    ...article, 
-                    tags: e.target.value
-                })
-              }
-              >
-              </input>
-              <br/>
-              <button onClick={postArticle} type="submit" className="btn btn-dark text-light col-2 mt-2">Submit</button>
-          </div>
-          </form>
-         
-      
-       
-       
-      </div>
-    </div>
-
-        </div>
       </div>
     </div>
     </div>
---
+++ src/components/dashboard/PostArticleForm/PostArticleForm.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import { useRef } from 'react';
+
+const PostArticleForm = ({user,allArticles,setArticles,postArticle,article,setArticle, img, setImg,imgLink, setImgLink,
+onImgChange}) => {
+
+  
+
+const ref = useRef();
+  return (
+    <div class="col-md">
+      <div class="card bg-light text-dark">
+        <div class="card-body text-center">
+          <div class="h1 mb-1">{/* <Icon.ArrowRight /> */}</div>
+          <h5 class="card-title mb-1">Post Article</h5>
+          <form class="row g-3">
+            <div id="post-article-form-box" class="col-md-12">
+              {/* <br/> */}
+              <label for="comments" class="form-label"></label>
+
+        
+
+              <input
+                class="form-control title-place"
+                type="text"
+                name="title"
+                rows="2"
+                required
+                placeholder="Title"
+                pattern="[A-Za-z]{2,}"
+                value={article.title}
+                onChange={(e) =>
+                  setArticle({
+                    ...article,
+                    title: e.target.value,
+                  })
+                }
+              ></input>
+              <label for="comments" class="form-label"></label>
+              <input
+                class="form-control body-place"
+                rows="10"
+                cols="40"
+                type="text"
+                name="contributor"
+                required
+                placeholder="Contributor"
+                value={article.contributor}
+                onChange={(e) =>
+                  setArticle({
+                    ...article,
+                    contributor: e.target.value,
+                  })
+                }
+              ></input>
+              <label for="comments" class="form-label"></label>
+              <textarea
+                class="form-control body-place"
+                rows="10"
+                cols="40"
+                type="text"
+                name="body"
+                required
+                placeholder="Article body here"
+                value={article.body}
+                onChange={(e) =>
+                  setArticle({
+                    ...article,
+                    body: e.target.value,
+                  })
+                }
+              ></textarea>
+              <label for="comments" class="form-label"></label>
+              <input
+                class="form-control body-place"
+                rows="10"
+                cols="40"
+                type="text"
+                name="tags"
+                required
+                placeholder="Tags"
+                value={article.tags}
+                onChange={(e) =>
+                  setArticle({
+                    ...article,
+                    tags: e.target.value,
+                  })
+                }
+              ></input>
+              <br />
+              <input type='file' onChange={onImgChange} ref={ref} />
+              <img src={img}  style={{width:"200px"}}/>
+              <button
+                onClick={postArticle}
+                type="submit"
+                className="btn btn-dark text-light col-2 mt-2"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostArticleForm;
