c6f9c11b68436efc2884d97ef1d73cfe5ee04803
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-25 22:23:42 -0400

    Deploy

---
+++ build/precache-manifest.8831b1735d73e3657b913b89fa995714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c09f5545187fc87be3a3",
+    "url": "/static/css/main.c155839c.chunk.css"
+  },
+  {
+    "revision": "c09f5545187fc87be3a3",
+    "url": "/static/js/main.c09f5545.chunk.js"
+  },
+  {
+    "revision": "9b47298707f365d7f274",
+    "url": "/static/js/1.9b472987.chunk.js"
+  },
+  {
+    "revision": "022e9d808f648d6ba26c",
+    "url": "/static/js/2.022e9d80.chunk.js"
+  },
+  {
+    "revision": "78f8f7d2fe648c5aa332",
+    "url": "/static/js/runtime~main.78f8f7d2.js"
+  },
+  {
+    "revision": "b8ac06aca99f931e4904da60724e7cc5",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import AuthPage from './pages/dashboard/AuthPage/AuthPage';
 import AdminDashboardPage from './pages/dashboard/AdminDashboardPage/AdminDashboardPage';
 import MagazinePage from './pages/dashboard/MagazinePage/MagazinePage';
@@ -50,18 +50,20 @@ function App() {
   //   }
   // }
 
-  async function getData() { 
-    try { 
-    let response1 = await fetch('/api/contributorSubmissions/allContributors') 
-    let contributors = await response1.json() 
-    setContributors(contributors) 
-    let response2 = await fetch('/api/articleSubmissions/allArticles') 
-    let articles = await response2.json() 
-    setArticles(articles) } catch(err) { 
-      console.log("couldn't fetch posts") 
-    } 
+  async function getData() {
+    try {
+      let response1 = await fetch(
+        '/api/contributorSubmissions/allContributors'
+      );
+      let contributors = await response1.json();
+      setContributors(contributors);
+      let response2 = await fetch('/api/articleSubmissions/allArticles');
+      let articles = await response2.json();
+      setArticles(articles);
+    } catch (err) {
+      console.log("couldn't fetch posts");
+    }
   }
-
 
   const verifyUserToken = () => {
     let token = localStorage.getItem('token');
@@ -91,23 +93,35 @@ function App() {
     >
       <div className="App">
         <Routes>
-          <Route path="/login" element={user !== null ? (<Navigate to="/dashboard" />
+          <Route
+            path="/login"
+            element={
+              user !== null ? (
+                <Navigate to="/dashboard" />
               ) : (
-              <AuthPage user={user} setUserInState={setUserInState} />
+                <AuthPage user={user} setUserInState={setUserInState} />
               )
             }
           />
-          <Route path="/" element={
-              <MagazinePage  allArticles={articles} allContributors={contributors} user={user}
+          <Route
+            path="/"
+            element={
+              <MagazinePage
+                allArticles={articles}
+                allContributors={contributors}
+                user={user}
               />
             }
           />
           <Route path="*" element={<Navigate to="/" replace />} />
           <Route path="/latest" element={<Latest allArticles={articles} />} />
-          <Route path="/detail"
+          <Route
+            path="/detail"
             element={<ArticleDetail allArticles={articles} />}
           />
-          <Route path="/dashboard" element={
+          <Route
+            path="/dashboard"
+            element={
               <AdminDashboardPage
                 user={user}
                 handleLogOut={handleLogOut}
