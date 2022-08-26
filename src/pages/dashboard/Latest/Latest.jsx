3f250c7cbc36e716bd3a51b47c1f14e934934d4b
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-26 12:03:50 -0400

    test push

--- src/pages/dashboard/Latest/Latest.jsx
+++ src/pages/dashboard/Latest/Latest.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 const Latest = (props) => {
 
   const articlesReversed = props.allArticles.reverse()
+  // hello hello
 
     return(    
     <div class="container my-5">
