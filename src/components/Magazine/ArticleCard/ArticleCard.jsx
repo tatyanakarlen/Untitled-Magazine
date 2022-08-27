4b4185b367cf32f560d1e06e258bf0a3933d56fc
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-27 07:51:09 -0400

    login page done

--- src/components/Magazine/ArticleCard/ArticleCard.jsx
+++ src/components/Magazine/ArticleCard/ArticleCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-
+import './ArticleCard.css'
 const ArticleCard = (props) => {
   // {title, contributor, body, image, createdAt}
   const navigate = useNavigate();
