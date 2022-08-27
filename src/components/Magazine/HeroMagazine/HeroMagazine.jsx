49b4c6abab3ca9d4528127bed9342f600e46cb94
tatyanakarlen <99438264+tatyanakarlen@users.noreply.github.com>
2022-08-26 23:03:32 -0400

    fixed hamburger

--- src/components/Magazine/HeroMagazine/HeroMagazine.jsx
+++ src/components/Magazine/HeroMagazine/HeroMagazine.jsx
@@ -1,20 +1,40 @@
 import React from 'react'
 
-const HeroMagazine = () => {
+const HeroMagazine = ({hamburgerBtnActive}) => {
+
+  let heading = ''
+  if (!hamburgerBtnActive) {
+    heading = <div>
+    <h1>UNTITLED<span class="text-warning">magazine</span></h1>
+    <p class="lead my-3">
+      Your best source for daily content from our print edition
+    </p>
+    </div>
+  }
+
+  //  heading = <div>
+  // <h1>UNTITLED<span class="text-warning">magazine</span></h1>
+  // <p class="lead my-3">
+  //   Your best source for daily content from our print edition
+  // </p>
+  // </div>
+
+
   return (
     <section
       class="bg-dark text-light p-5 p-lg-0 pt-lg-5 text-center text-sm-start"
     >
       <div class="container">
         <div class="d-sm-flex align-items-center justify-content-between" style={{ marginTop: '4rem'}}>
-          <div>
+          {heading}
+          {/* <div>
             <h1>UNTITLED<span class="text-warning">magazine</span></h1>
             <p class="lead my-3">
               Your best source for daily content from our print edition
             </p>
            
            
-          </div>
+          </div> */}
       
         </div>
       </div>
